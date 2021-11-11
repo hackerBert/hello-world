be408e33e2067c849cf91e272e6f32a04b1e6204
hackerBert <bertan.bayaltun@ugent.be>
2021-11-11 11:40:06 +0100

    edit3 test

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo");
         System.out.println("hallo2");
+        System.out.println("hallo3");
     }
 }
